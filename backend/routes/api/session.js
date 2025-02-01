3985591101c6b71c279d3e6ecf6ee9c7de47ae3e
WhiteyOne <Cayden.Hanks.Pro@gmail.com>
2025-01-31 22:18:58 -0800

    sign up/sign out/signup

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -14,40 +14,47 @@ const router = express.Router();
 
 // 
 // Log in
-router.post(
-    '/',
-    async (req, res, next) => {
-      const { credential, password } = req.body;
-  
-      const user = await User.unscoped().findOne({
+router.post('/', async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.unscoped().findOne({
         where: {
-          [Op.or]: {
-            username: credential,
-            email: credential
-          }
+            [Op.or]: {
+                username: credential,
+                email: credential
+            }
         }
-      });
-  
-      if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+    });
+
+    if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
         const err = new Error('Login failed');
         err.status = 401;
         err.title = 'Login failed';
         err.errors = { credential: 'The provided credentials were invalid.' };
         return next(err);
-      }
-  
-      const safeUser = {
+    }
+
+    const safeUser = {
         id: user.id,
         email: user.email,
         username: user.username,
-      };
-  
-      await setTokenCookie(res, safeUser);
-  
-      return res.json({
+    };
+
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
         user: safeUser
-      });
-    }
-  );
+    });
+}
+);
+
+
+// Log out
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+}
+);
+
 
 module.exports = router;
