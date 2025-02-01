8b0c2ffbcd77d36d000836eb3b9fa2ccb641682d
WhiteyOne <Cayden.Hanks.Pro@gmail.com>
2025-01-31 21:57:33 -0800

    login auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,8 @@
 
 //express imports
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 //sequelize imports
 const { User } = require('../../db/models');
 
@@ -13,6 +15,15 @@ const { restoreUser, setTokenCookie, requireAuth} = require('../../utils/auth.js
 //Middleware
 router.use(restoreUser);
 
+//Routes for API
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+  });
 
 
 module.exports = router;
